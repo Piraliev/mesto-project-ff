fa1f032b24ace2225288b51ce9a771f451e92438
Ruslan Piraliev <piraliev@gmail.com>
2024-10-10 17:28:10 +0300

    Project 5. JS DOM

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template');
+const cardTemplateContent = cardTemplate.content;
+
 // @todo: DOM узлы
+
+const cardPlace = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
 
+function cardCreate(element) {
+  const cardTitle = element.name;
+  const cardLink = element.link;
+  const cardTemplateClone = cardTemplateContent.cloneNode(true);
+  cardTemplateClone.querySelector('.card__title').textContent = cardTitle;
+  cardTemplateClone.querySelector('.card__image').setAttribute('src', cardLink);
+  const cardItem = cardTemplateClone.querySelector('.places__item');
+  const deleteButton = cardTemplateClone.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => cardDelete(cardItem));
+  return cardTemplateClone;
+}
+
 // @todo: Функция удаления карточки
 
+function cardDelete(card) {
+  card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((card) => {
+  cardPlace.append(cardCreate(card));
+})
